1d7cae99f75b9c92d387c7cd247dc7165c7c3238
raflyryhsnyh <rayhansyahrafly@gmail.com>
2025-12-18 12:02:49 +0700

    fix: table todos in migrations

--- database/migrations/2025_12_18_032950_create_todos_table.php
+++ database/migrations/2025_12_18_032950_create_todos_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->enum('status', ['todo', 'doing', 'done'])->default('todo');
             $table->integer('order')->default(0);
+            $table->date('due_date')->nullable();
             $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
